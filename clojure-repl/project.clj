28b11798ad0d1a8bbdeeba00a5ac9e2ffdd127c5
Dylan Wilson <dylanjw@protonmail.com>
2019-02-12 17:39:44 +0900

    update project file

--- clojure-repl/project.clj
+++ clojure-repl/project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-repl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple project to test out repl"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "https://github.com/dylanjw/clojure-repl"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
